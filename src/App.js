97a4e1ca098c56a9738deea8a257c3ba4ebcc10e
Mahesh Srisailam <srisailammahesh77@gmail.com>
2022-07-05 14:00:17 +0000

    Assessment

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import VideoPlayer from './components/VideoPlayer'
+import './App.css'
+
+const App = () => <VideoPlayer />
+export default App
---
+++ src/components/VideoItem/index.js
@@ -0,0 +1,21 @@
+import ReactPlayer from 'react-player'
+
+import './index.css'
+
+const VideoItem = props => {
+  const {videoDetails, onChangeVideo} = props
+  const {title, URL} = videoDetails
+  const onClickVideoItem = () => {
+    onChangeVideo(videoDetails)
+  }
+  return (
+    <li onClick={onClickVideoItem}>
+      <ReactPlayer url={URL} controls height="100px" width="150px" />
+      <div>
+        <p className="title">{title}</p>
+      </div>
+    </li>
+  )
+}
+
+export default VideoItem
