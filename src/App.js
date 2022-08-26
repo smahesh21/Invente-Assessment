0c0b47d14cd2ce778be277aad6c14b0d8a796d63
Mahesh <96672896+smahesh21@users.noreply.github.com>
2022-08-26 10:10:24 +0530

    Added comment

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import VideoPlayer from './components/VideoPlayer'
 import './App.css'
-
+//This is a component which wraps all the child components
 const App = () => <VideoPlayer />
 export default App
